488fde2fa0b1f301a19d94933b22699bcf57072d
Borje123 <93801825+Borje123@users.noreply.github.com>
2022-11-18 20:44:28 +0800

    Delete Django1 - Add Record/covtrace_test/covtrace_test/covtrace_app directory

--- Django1 - Add Record/covtrace_test/covtrace_test/covtrace_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path 
-from . import views
-
-#URLConf
-urlpatterns = [
-    path('hello/', views.say_hello, name="home"),
-    path('join/', views.Add_Patient, name="join"),
-]
