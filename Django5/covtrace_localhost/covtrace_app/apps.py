b0c6096cbf09563c33db4c8afdd6e5e12150dcb9
Xenia Velacruz <99013566+shibalshi@users.noreply.github.com>
2022-11-22 06:16:50 +0000

    Add files via upload

---
+++ Django5/covtrace_localhost/covtrace_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CovtraceAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'covtrace_app'
---
+++ Django5/covtrace_localhost/covtrace_app/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from .forms import PatientForm
+from .models import Patient 
+import folium 
+from folium import plugins
+
+
+#calling the url
+def home(request):
+    #calling all data in database
+    
+    all_patients = Patient.objects.all
+
+    return render(request,"dashboard/index.html", {'all': all_patients})
+
+def Add_Patient(request):
+    if request.method == "POST":
+        form = PatientForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+        return render(request, 'dashboard/add_record.html', {}) 
+    else:
+        return render(request, 'dashboard/add_record.html', {})  
+
+def Content(request):
+    return render(request, "dashboard/hello.html", {})
+
+def Map(request):
+    data = Patient.objects.all()
+    data_list = Patient.objects.values_list('latitude', 'longitude', 'population')
+    
+    map1 = folium.Map( location=[14.116667, 122.949997], tiles="CartoDB Dark_Matter",  zoom_start=14)
+    
+    plugins.HeatMap(data_list).add_to(map1)
+    plugins.Fullscreen().add_to(map1)
+    map1 = map1._repr_html_()
+    context = {
+        'map1': map1
+    }
+    return render(request, 'dashboard/map.html', context)
+
+def News(request):
+    return render(request,"dashboard/news_about_covid.html", {})
