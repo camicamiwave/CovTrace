33457a498dd080472183a0df1a62c7fdeeee7c95
John Erwin S. Albos <98995938+riyuwin@users.noreply.github.com>
2022-12-01 09:19:15 +0800

    Delete covtrace_localhost/Home directory

--- covtrace_localhost/Home/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from Home import views
-
-urlpatterns = [
-    path('', views.Home, name='home'),
-    path('get_recovered_patient/<patient_id>', views.Get_Recovered_Patient, name='get_recovered_patient'),
-]
